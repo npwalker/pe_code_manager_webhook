bb8e2f669f2673969c06ac2f1863abad5e231507
Nick Walker <nick.walker@puppetlabs.com>
2016-02-28 07:21:38 -0800

    Add a check to see if code manager is enabled via NC or hiera
    Prior to this commit the determination of whether code manager
    or zack/r10k should be configured was based on PE version.
    
    After this commit, since code manager needs to be enabled for all
    of this to work, we'll check if code manager is enabled via the
    PE console UI or via hiera and then only proceed with configuring
    code manager if that is the case.
    
    If you are using 2015.3+ and have not enabled code manager yet then
    you will be notified to do so instead of automatically installing
    zack/r10k.  Use $force_zack_r10k_webhook if you prefer.

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,8 +2,16 @@ class pe_git_webhook (
   $force_zack_r10k_webhook = false
 ) {
 
+  #Determine if code manager is enabled
+  $code_manager_auto_configure_nc_value    = node_groups('PE Master')['PE Master']['classes']['puppet_enterprise::profile::master']['code_manager_auto_configure']
+  $code_manager_auto_configure_hiera_value = hiera('puppet_enterprise::profile::master::code_manager_auto_configure')
+  #The NC value will take precedence over the hiera value, if neither is set then it defaults to false
+  $code_manager_auto_configure             = pick($code_manager_auto_configure_nc_value, $code_manager_auto_configure_hiera_value, false)
+
   if versioncmp( $::pe_server_version, '2015.2.99' ) <= 0 or $force_zack_r10k_webhook {
     include pe_git_webhook::zack_r10k_webhook
+  } elsif versioncmp( $::pe_server_version, '2015.2.99' ) > 0 and $code_manager_auto_configure == false {
+    notify { "Please Enable Code Manager via the PE Console UI or hiera with this parameter `puppet_enterprise::profile::master::code_manager_auto_configure`"}
   } else {
     include pe_git_webhook::code_manager
     include pe_git_webhook::zack_r10k_webhook_disable
